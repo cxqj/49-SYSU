55bb3a935189be3ab5007e4cb2cd73bfbad71b4d
cxqj <43702702+cxqj@users.noreply.github.com>
2020-10-30 17:08:11 +0800

    Update EventEncoder.py

--- models/EventEncoder.py
+++ models/EventEncoder.py
@@ -92,7 +92,7 @@ class TSRM_Encoder(Basic_Encoder):
 
     """
     feats: (B,T,512)
-    vid_idx: [0,0,0,..,Prop_N]
+    vid_idx: [0,0,0,..,Prop_N]  指示当前proposal属于哪一个batch
     featstamps:  (Prop_N,2)
     event_seq_idx: [0,1,2,....,Prop_N]
     timestamps: [[0,7.67],[7.67,31.94],[31.94,50.25],[50.25,61.32],[61.32,73.67],[73.67,81.25]]
@@ -100,14 +100,15 @@ class TSRM_Encoder(Basic_Encoder):
     """
     def forward(self, feats, vid_idx, featstamps, event_seq_idx, timestamps, vid_time_len):
         clip_feats, clip_mask = self.get_clip_level_feats(feats, vid_idx, featstamps)  # (Prop_N, max_event_len, 512), (Prop_N, max_event_len)
+        ###### 对应semantic ralation branch的mean pooling操作 ####
         event_feats = (clip_feats * clip_mask.unsqueeze(2)).sum(1) / (clip_mask.sum(1, keepdims=True) + 1e-5)  # (Prop_N, max_event_len, 512)-->(Prop_N, 512)
         event_feats = self.pre_map(event_feats)   # (Prop_N, 512)-->(Prop_N,512)
 
         batch_size = len(event_seq_idx)  # 1
         event_num = sum([_.shape[0] * _.shape[1] for _ in event_seq_idx])  # event_seq_idx:(1,Prop_N)
         event_feats_expand = feats.new_zeros(event_num, event_feats.shape[-1])  # (Prop_N,512)
-        mask = feats.new_zeros(event_num, event_num)  # (Prop_N,Prop_N)
-        timestamp_expand = []
+        mask = feats.new_zeros(event_num, event_num)  # (Prop_N,Prop_N)  因为要计算当前proposal和其它proposal的关系，因此需要拓展mask
+        timestamp_expand = []   
         vid_time_len_expand = []
 
         total_idx = 0
@@ -121,7 +122,10 @@ class TSRM_Encoder(Basic_Encoder):
                 vid_time_len_expand.extend([vid_time_len[i].item() for jj in event_idx])  # [85.16,85.16,...,85.16]
                 total_idx += len(event_idx)
 
-        # 这里用的就是transformer的结构，group相当于multi_head
+        """
+         计算不同提议间的语义相似度
+         adopt scaled dot-product attention to calculate the semantic similarity between proposals.
+        """
         query_mat = self.query_map(event_feats_expand).reshape(event_num, self.group,
                                                      int(self.hidden_dim / self.group)).transpose(0, 1)  # (Prop_N,512)-->(Prop_N,16,32)-->(16, Prop_N, 32)
         key = self.key_map(event_feats_expand).reshape(event_num, self.group, int(self.hidden_dim / self.group)).transpose(0,
@@ -131,16 +135,18 @@ class TSRM_Encoder(Basic_Encoder):
         cos_sim = cos_sim / math.sqrt(self.hidden_dim / self.group)  
         sim = cos_sim
         mask = mask.unsqueeze(0)  # (1,Prop_N,Prop_N)
-
-        ###### 对于论文中Temporal Relation 分支  ######
+    
+        """
+        计算不同提议间的时序相关性
+        对应论文中Temporal Relation 分支  ######
+        """
         if self.use_posit_branch:
             pos_matrix = extract_position_matrix(np.array(timestamp_expand), event_num)   # (Prop_N,2) 
-            pos_feats = extract_position_embedding(pos_matrix,
-                                                   self.posit_hidden_dim)  # [Prop_N, Prop_N, 512]
+            pos_feats = extract_position_embedding(pos_matrix,self.posit_hidden_dim)  # [Prop_N, Prop_N, 512]
             pos_feats = feats.new_tensor(pos_feats).reshape(-1, self.posit_hidden_dim)  # (Prop_N x Prop_N, 512)
             pos_sim = self.pair_pos_fc2(torch.tanh(self.pair_pos_fc1(pos_feats))).reshape(event_num, event_num, self.group)  # 经过两个全连接层编码 (Prop_N, Prop_N, 16)
             pos_sim = pos_sim.permute(2, 0, 1)   # (16,Prop_N,Prop_N)
-            sim = cos_sim + pos_sim   # (16,Prop_N,Prop_N)
+            sim = cos_sim + pos_sim   # (16,Prop_N,Prop_N)    融合语义和时序相似度信息
 
         sim = F.softmax(sim, dim=2)
         sim = (sim * mask) / (1e-5 + torch.sum(sim * mask, dim=2, keepdim=True))   # (16,Prop_N,Prop_N)
@@ -148,8 +154,8 @@ class TSRM_Encoder(Basic_Encoder):
         value = self.value_map(event_feats_expand).reshape(event_num, self.group, -1).transpose(0, 1)  # (Prop_N,512)-->(Prop_N,16,32)-->(16,Prop_N,32)
         event_ctx = torch.bmm(sim, value)  # (16, Prop_N, Prop_N) x (16, Prop_N, 32)-->(16, Prop_N, 32)
         event_ctx = event_ctx.transpose(0, 1).reshape(event_num, -1)  # (Prop_N,512)
-        event_ctx = torch.cat((F.relu(event_ctx), event_feats_expand), 1)  # (Prop_N,1024)
-        event_ctx = self.drop(event_ctx)  
+        event_ctx = torch.cat((F.relu(event_ctx), event_feats_expand), 1)  # (Prop_N,1024)    融合mean pooling特征
+        event_ctx = self.drop(event_ctx)    # 最终的事件特征
 
         # positional feature vector for each event
         pos_feats = feats.new(len(timestamp_expand), 100).zero_()  # (Prop_N,100)
@@ -160,6 +166,7 @@ class TSRM_Encoder(Basic_Encoder):
             pos_feats[i, s: e + 1] = 1
         event_ctx = torch.cat([event_ctx, pos_feats], dim=1)  # (Prop_N,1024) + (Prop_N,100)-->(Prop_N,1124)
 
+        ### event_cxt = weighted_event_feature + event_feats_expand + positional_feature_vector
         return event_ctx, clip_feats, clip_mask   # (Prop_N,1124), (Prop_N, max_event_len, 512), (Prop_N, max_event_len)
 
 
